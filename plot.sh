a56abe6d53c166d2508be8bb3109246a732fb66c
peter.holst.andersen <peter@ubuntu>
2017-05-17 10:35:32 +0200

    fip

--- plot.sh
+++ plot.sh
@@ -5,14 +5,17 @@ cat $@ |\
     grep actual |\
     sed 's/actual\[.\]=//g' |\
     sed 's/target_temperature=//g' |\
-    awk 'BEGIN{ count = 0;}{ print count++, $0;}' |\
-    ./avg.py > /tmp/to_plot
+    awk 'BEGIN{ count = 0;}{ print count++, $0;}' \
+    | ./avg.py \
+    > /tmp/to_plot
+
 
 # "/tmp/to_plot" using 3 with line title "actual[0]",\
+# "/tmp/to_plot" using 5 with line title "actual[2]",\
 
 gnuplot > /tmp/graph.png <<FOO
 set terminal png
-set yrange [8:16]
+set yrange [8:18]
 plot \
      "/tmp/to_plot" using 3 with line title "actual[0]",\
      "/tmp/to_plot" using 4 with line title "actual[1]",\
