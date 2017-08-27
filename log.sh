7d87d30760e095785c55fd209f37c23964fd877f
Peter <peter@localhost>
2017-08-27 20:09:35 +0200

    log.sh

--- log.sh
+++ log.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-if [ ! $EUID == "0" ] then
+if [ ! $EUID == "0" ]; then
    echo Must run as root
    exit
 fi
 	
-chmod 0666 /dev/ttyACM0
+chmod 0666 /dev/ttyUSB0
 
 while [ true ]; do
-    stty -F /dev/ttyACM0 -hupcl
-    /home/peter/bin/ttylog -b 115200 -d /dev/ttyACM0 -f
+    stty -F /dev/ttyUSB0 -hupcl
+    /home/peter/bin/ttylog -b 115200 -d /dev/ttyUSB0 -f
     # ttylog -b 115200 -d /dev/ttyACM0 -f |\
 	# gawk '{ print strftime("%c  [%s] "), $0; fflush(); }'
 	# awk '{ print $8; fflush(); }'
